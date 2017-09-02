7bc1561b5bdf21daedb8d1d91203606ebd2ca719
PRISCILLA DE  OLIVEIRA <ALU201731567@uniritter.local>
2017-09-01 22:17:53 -0300

    criados cenários de test

---
+++ src/test/java/br/com/velhoBarreiro/ExecutaCriarEvento.java
@@ -0,0 +1,5 @@
+package br.com.velhoBarreiro;
+
+public class ExecutaCriarEvento {
+
+}
---
+++ src/test/java/br/com/velhoBarreiro/US001_CRIAR_EVENTO.java
@@ -0,0 +1,25 @@
+package br.com.velhoBarreiro;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+
+public class US001_CRIAR_EVENTO {
+  @Given("^you are in Given annotation$")
+  public void given() throws Throwable {
+  }
+
+  @When("^you are in When annotation$")
+  public void when() throws Throwable {
+  }
+
+  @Then("^you are in Then annotation$")
+  public void then() throws Throwable {
+  }
+
+  @And("^you are in And annotation$")
+  public void and() throws Throwable {
+  }
+
+}

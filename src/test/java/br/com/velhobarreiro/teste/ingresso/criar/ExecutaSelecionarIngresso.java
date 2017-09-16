851b5421d5b545e8ad0f1d00f038da8be21a642f
ALLAN MOREIRA LEITE <ALU201410259@uniritter.local>
2017-09-15 21:52:48 -0300

    US002 Selecionar Ingresso

---
+++ src/test/java/br/com/velhobarreiro/teste/ingresso/criar/ExecutaSelecionarIngresso.java
@@ -0,0 +1,11 @@
+package br.com.velhobarreiro.teste.ingresso.criar;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/Funcionalidades/Ingresso/US002 Selecionar Ingresso.feature")
+public class ExecutaSelecionarIngresso {
+}
